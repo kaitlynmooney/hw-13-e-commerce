2c69bb21431637cc7e4302dec5ef6d652a952675
kaitlynmooney <kmooney806@gmail.com>
2024-07-13 21:03:18 -0400

    added routes

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 // import models
-const Product = require('../Product');
-const Category = require('../Category');
-const Tag = require('../Tag');
-const ProductTag = require('../ProductTag');
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
@@ -15,14 +15,14 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
   },
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
   },
