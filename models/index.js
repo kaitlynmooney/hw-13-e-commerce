9ae96720b6a37b07daec74b2b403ff8f6a5a337f
kaitlynmooney <kmooney806@gmail.com>
2024-07-13 20:30:53 -0400

    added index.js model

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('../Product');
+const Category = require('../Category');
+const Tag = require('../Tag');
+const ProductTag = require('../ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
 
+});
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 module.exports = {
   Product,
